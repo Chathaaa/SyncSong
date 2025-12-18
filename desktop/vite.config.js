b1eefc0b33ef5c8f22e3e671df8731ed6dc98e38
Chathaaa <shawnchatha@gmail.com>
2025-12-17 19:08:27 -0800

    First working version

--- desktop/vite.config.js
+++ desktop/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "renderer",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "renderer/index.html",
+        spotifyCallback: "renderer/spotify-callback.html",
+      },
+    },
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
